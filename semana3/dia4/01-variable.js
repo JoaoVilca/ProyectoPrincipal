bc1acb34bd4b41f7f94c31193a740790023059fa
Joao Vilca <joaovilca@gmail.com>
2020-09-18 22:16:09 -0500

    clases semana 3

---
+++ semana3/dia4/01-variable.js
@@ -0,0 +1,8 @@
+let nombre = "Joao"
+let apellido = "Vilca"
+let edad = 25
+let dechaNacimiento = "09-08-1991"
+
+console.log(nombre);
+console.log("nombre");
+console.log(`nombre ${nombre} y apellido ${apellido}`)
